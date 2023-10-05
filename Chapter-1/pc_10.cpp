fe49b006f9a1656f419a477b7c440e812ae5281e
NatsuDrag9 <rohitimandi9@zoho.com>
2023-10-05 10:15:45 +0200

    Added solution to question ten

---
+++ Chapter-1/pc_10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    cout << "Name: Natsu Dragneel" << endl 
+        << "Address: Denmark, Europe, Earth" << endl 
+        << "Telephone: " << 12345678 << endl 
+        << "College Major: Engineering" << endl;
+
+    return 0;
+}
