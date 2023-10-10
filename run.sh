757af3cc9b136aa173fbe6a787f0730f0ca3de05
NatsuDrag9 <rohitimandi9@zoho.com>
2023-10-10 12:26:15 +0200

    Automated shell script execution in each chapter

--- run.sh
+++ run.sh
@@ -1,7 +1,30 @@
 #!/bin/bash
 # Script to build all files
 
-####### Chapter 2 #######
-cd Chapter-2
-chmod +x run_executables.sh
-./run_executables.sh
+PROJ_DIR="."
+
+# Check if the project directory exists
+if [ -d "${PROJ_DIR}" ]; then
+    # Loop through the sub-directories
+    for CHP_DIR in "${PROJ_DIR}"/Chapter-*; do
+        # Check if the chapter directory exists
+        if [ -d "${CHP_DIR}" ]; then
+            # Check if run_executables.sh exists in CHP_DIR
+            if [ -f "${CHP_DIR}/run_executables.sh" ]; then
+                # Change to CHP_DIR
+                cd ${CHP_DIR}
+                echo "Running run_executables in ${CHP_DIR}"
+                ./run_executables.sh
+
+                # Return to original directory
+                cd - || exit
+            else
+                echo "run_executables.sh not found in ${CHP_DIR}"
+            fi
+        else
+            echo "${CHP_DIR} not found."
+        fi
+    done
+else
+    echo "Project directory not found."
+fi
