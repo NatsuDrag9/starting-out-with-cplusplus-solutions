b8d337c706c68f890e4ba95867c5b19e9f7bed9e
NatsuDrag9 <rohitimandi9@zoho.com>
2023-10-10 14:49:26 +0200

    Finished question 11

---
+++ Chapter-3/pc_11.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+    float celciusTemp, fahrenheitTemp;
+
+    cout << "Enter temperature in celcius: ";
+    cin >> celciusTemp;
+
+    fahrenheitTemp = (9.0/5.0)*celciusTemp + 32.0;
+
+    cout << celciusTemp << " degree C = " << fahrenheitTemp << " degree F" << endl;
+
+    return 0;
+}
