6777673175fb9f6bc6b8eed27ca36d42c177793f
NatsuDrag9 <rohitimandi9@zoho.com>
2023-10-12 12:49:05 +0200

    Finished question 10

---
+++ Chapter-5/pc_10.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(void)
+{
+    const int min = 1;
+    const int max = 100;
+
+    // Seed random number with the current time
+    srand((static_cast<unsigned>(time(nullptr))));
+
+    // Generate random number
+    int randomNumber = min + rand() % max;
+
+    // Run llop until the user guesses correctly
+    while (1)
+    {
+        int userNum;
+        cout << "Enter your guess: ";
+        cin >> userNum;
+        if (userNum < randomNumber)
+        {
+            cout << "Too low. Try again" << endl;
+            continue;
+        }
+        else if (userNum > randomNumber)
+        {
+            cout << "Too high. Try again" << endl;
+            continue;
+        }
+        else if (userNum == randomNumber)
+        {
+            cout << "Congratulations! You figured out my number." << endl;
+            break;
+        }
+    }
+
+    return 0;
+}
