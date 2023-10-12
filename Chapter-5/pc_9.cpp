92cf10ccccd3db2f738039551338b424d7b048b7
NatsuDrag9 <rohitimandi9@zoho.com>
2023-10-12 12:37:16 +0200

    Finished question 9

--- Chapter-5/pc_9.cpp
+++ Chapter-5/pc_9.cpp
@@ -3,7 +3,13 @@ using namespace std;
 
 int main(void)
 {
-    
+    int annualMembershipFee = 2500, time = 6;
+    float increaseFraction = 0.04, annualIncrease = annualMembershipFee * increaseFraction;
+
+    for(int i = 1; i <= time; i++){
+        cout << "Projected rate after year " << i << ": " << annualIncrease + annualMembershipFee << endl;
+        annualIncrease += (annualIncrease + annualMembershipFee)*increaseFraction;
+    }
 
     return 0;
 }
