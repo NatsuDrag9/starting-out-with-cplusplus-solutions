3b5fea37c5b88b7c7ba6146b91d059262749f493
NatsuDrag9 <rohitimandi9@zoho.com>
2023-10-13 15:38:58 +0200

    Commented block to prevent inifinite loop in workflow

--- Chapter-5/pc_11.cpp
+++ Chapter-5/pc_11.cpp
@@ -5,41 +5,44 @@ using namespace std;
 
 int main(void)
 {
-    const int min = 1;
-    const int max = 100;
+    /*
+  This block was commented for to prevent github actions workflow from entering into an infinite loop. Uncomment it to run the program
+  */
+    // const int min = 1;
+    // const int max = 100;
 
-    // Seed random number with the current time
-    srand((static_cast<unsigned>(time(nullptr))));
+    // // Seed random number with the current time
+    // srand((static_cast<unsigned>(time(nullptr))));
 
-    // Generate random number
-    int randomNumber = min + rand() % max;
-    int guessCount = 0;
+    // // Generate random number
+    // int randomNumber = min + rand() % max;
+    // int guessCount = 0;
 
-    // Run llop until the user guesses correctly
-    while (1)
-    {
-        int userNum;
-        cout << "Enter your guess: ";
-        cin >> userNum;
-        if (userNum < randomNumber)
-        {
-            cout << "Too low. Try again" << endl;
-            guessCount++;
-            continue;
-        }
-        else if (userNum > randomNumber)
-        {
-            cout << "Too high. Try again" << endl;
-            guessCount++;
-            continue;
-        }
-        else if (userNum == randomNumber)
-        {
-            cout << "Guess count: " << guessCount << endl;
-            cout << "Congratulations! You figured out my number." << endl;
-            break;
-        }
-    }
+    // // Run llop until the user guesses correctly
+    // while (1)
+    // {
+    //     int userNum;
+    //     cout << "Enter your guess: ";
+    //     cin >> userNum;
+    //     if (userNum < randomNumber)
+    //     {
+    //         cout << "Too low. Try again" << endl;
+    //         guessCount++;
+    //         continue;
+    //     }
+    //     else if (userNum > randomNumber)
+    //     {
+    //         cout << "Too high. Try again" << endl;
+    //         guessCount++;
+    //         continue;
+    //     }
+    //     else if (userNum == randomNumber)
+    //     {
+    //         cout << "Guess count: " << guessCount << endl;
+    //         cout << "Congratulations! You figured out my number." << endl;
+    //         break;
+    //     }
+    // }
 
     return 0;
 }
