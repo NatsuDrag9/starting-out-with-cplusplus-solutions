550b69801fe01dfa104af1bff7bc273406494dc5
NatsuDrag9 <rohitimandi9@zoho.com>
2023-10-13 15:42:15 +0200

    Commented block to prevent inifite loop in worflow

--- Chapter-5/pc_24.cpp
+++ Chapter-5/pc_24.cpp
@@ -3,63 +3,66 @@ using namespace std;
 
 int main(void)
 {
-    int empNumber = 1, grossPay, stateTax, federalTax, fica;
+    /*
+  This block was commented for to prevent github actions workflow from entering into an infinite loop. Uncomment it to run the program
+  */
+    // int empNumber = 1, grossPay, stateTax, federalTax, fica;
 
-    // Fetching inputs from user
-    while (empNumber != 0)
-    {
-        cout << endl << "Enter employee number (or 0 to end input menu): ";
-        cin >> empNumber;
-        if(empNumber == 0){
-            break;
-        }
-        cout << "Enter gross pay (in $): ";
-        cin >> grossPay;
-        cout << "Enter state tax (in $): ";
-        cin >> stateTax;
-        cout << "Enter federal tax (in $): ";
-        cin >> federalTax;
-        cout << "Enter fica with holdings (in $): ";
-        cin >> fica;
-        if (stateTax + federalTax + fica > grossPay)
-        {
-            cout << "Sum of state tax, federal tax and fica with holdings cannot exceed gross pay. Reenter data for employee: " << empNumber << endl;
-        }
-    }
+    // // Fetching inputs from user
+    // while (empNumber != 0)
+    // {
+    //     cout << endl << "Enter employee number (or 0 to end input menu): ";
+    //     cin >> empNumber;
+    //     if(empNumber == 0){
+    //         break;
+    //     }
+    //     cout << "Enter gross pay (in $): ";
+    //     cin >> grossPay;
+    //     cout << "Enter state tax (in $): ";
+    //     cin >> stateTax;
+    //     cout << "Enter federal tax (in $): ";
+    //     cin >> federalTax;
+    //     cout << "Enter fica with holdings (in $): ";
+    //     cin >> fica;
+    //     if (stateTax + federalTax + fica > grossPay)
+    //     {
+    //         cout << "Sum of state tax, federal tax and fica with holdings cannot exceed gross pay. Reenter data for employee: " << empNumber << endl;
+    //     }
+    // }
 
-    // Input validation
-    if (empNumber < 0)
-    {
-        cout << "Invalid input! Employee number must be a positive number. Exiting..." << endl;
-        return 0;
-    }
-    if (grossPay < 0)
-    {
-        cout << "Invalid input! Gross pay must be a positive number. Exiting..." << endl;
-        return 0;
-    }
-    if (stateTax < 0)
-    {
-        cout << "Invalid input! State tax must be a positive number. Exiting..." << endl;
-        return 0;
-    }
-    if (federalTax < 0)
-    {
-        cout << "Invalid input! Federal tax must be a positive number. Exiting..." << endl;
-        return 0;
-    }
-    if (fica < 0)
-    {
-        cout << "Invalid input! Fica with holdings must be a positive number. Exiting..." << endl;
-        return 0;
-    }
+    // // Input validation
+    // if (empNumber < 0)
+    // {
+    //     cout << "Invalid input! Employee number must be a positive number. Exiting..." << endl;
+    //     return 0;
+    // }
+    // if (grossPay < 0)
+    // {
+    //     cout << "Invalid input! Gross pay must be a positive number. Exiting..." << endl;
+    //     return 0;
+    // }
+    // if (stateTax < 0)
+    // {
+    //     cout << "Invalid input! State tax must be a positive number. Exiting..." << endl;
+    //     return 0;
+    // }
+    // if (federalTax < 0)
+    // {
+    //     cout << "Invalid input! Federal tax must be a positive number. Exiting..." << endl;
+    //     return 0;
+    // }
+    // if (fica < 0)
+    // {
+    //     cout << "Invalid input! Fica with holdings must be a positive number. Exiting..." << endl;
+    //     return 0;
+    // }
 
-    // Payroll calculation
-    cout << endl << "Gross pay: $" << grossPay << endl;
-    cout << "State tax: $" << stateTax << endl;
-    cout << "Federal tax: $" << federalTax << endl;
-    cout << "FICA with holdings: $" << fica << endl;
-    cout << "Net pay: $" << grossPay - (stateTax + federalTax + fica) << endl;
+    // // Payroll calculation
+    // cout << endl << "Gross pay: $" << grossPay << endl;
+    // cout << "State tax: $" << stateTax << endl;
+    // cout << "Federal tax: $" << federalTax << endl;
+    // cout << "FICA with holdings: $" << fica << endl;
+    // cout << "Net pay: $" << grossPay - (stateTax + federalTax + fica) << endl;
 
     return 0;
 }
