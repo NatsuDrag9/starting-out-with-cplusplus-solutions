89022c5edd09378d559d558167f6ca13aba047c1
NatsuDrag9 <rohitimandi9@zoho.com>
2023-10-13 14:20:07 +0200

    Fixed compilation error

--- Chapter-5/pc_10.cpp
+++ Chapter-5/pc_10.cpp
@@ -5,18 +5,17 @@ using namespace std;
 
 int main(void)
 {
-    const int min = 1;
-    const int max = 100;
-
-    // Seed random number with the current time
-    srand((static_cast<unsigned>(time(nullptr))));
-
-    // Generate random number
-    int randomNumber = min + rand() % max;
-
     /*
    This block was commented for to prevent github actions workflow from entering into an infinite loop. Uncomment it to run the program
    */
+    // const int min = 1;
+    // const int max = 100;
+
+    // // Seed random number with the current time
+    // srand((static_cast<unsigned>(time(nullptr))));
+
+    // // Generate random number
+    // int randomNumber = min + rand() % max;
 
     // // Run loop until the user guesses correctly
     // while (1)
