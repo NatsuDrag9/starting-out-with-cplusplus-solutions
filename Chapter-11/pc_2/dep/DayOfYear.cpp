a3da5f10d47c7803e0e4bdc4f063ff2676db66bf
NatsuDrag9 <rohitimandi9@zoho.com>
2023-10-21 15:59:15 +0200

    Fixed typo in months array

--- Chapter-11/pc_2/dep/DayOfYear.cpp
+++ Chapter-11/pc_2/dep/DayOfYear.cpp
@@ -3,7 +3,7 @@
 #include "../inc/DayOfYear.h"
 
 // Defining static member variable
-MonthAndDays DayOfYear::months[] = {{"Januray", 31}, {"February", 28}, {"March", 31}, {"April", 30}, {"May", 31}, {"June", 30}, {"July", 31}, {"August", 31}, {"September", 30}, {"October", 31}, {"November", 30}, {"December", 31}};
+MonthAndDays DayOfYear::months[] = {{"January", 31}, {"February", 28}, {"March", 31}, {"April", 30}, {"May", 31}, {"June", 30}, {"July", 31}, {"August", 31}, {"September", 30}, {"October", 31}, {"November", 30}, {"December", 31}};
 
 /**
  * @brief Default constructor
